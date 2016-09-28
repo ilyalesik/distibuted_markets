ae112c5c17632bb0bea38d1b5e2e7d4b5117c746
Ilia Lesik <ilialesik@gmail.com>
2016-09-28 18:10:54 +0300

    calc tw

--- components/external_procedure.py
+++ components/external_procedure.py
@@ -9,7 +9,16 @@ def calc_delta_tw_stochastic_gradient(model, Q, eps):
     model_fix_t = model.get_data_model_with_fix_t(t)
     q = start_internal_procedure(model_fix_t, Q, eps)
     p = calc_p(model_fix_t, q)
-    return {k: 24 * (p[k[1]] - p[k[0]]) - (2 * v['a'] * Q[k] + v['b']) for k, v in model.edges.items()}
+    pr = reduce(lambda x, (k, v): x + 0.25 * p[k] ** 2 / v['A'], model_fix_t.nodes.items(), 0)
+    cs = reduce(lambda x, (k, v): x + 0.5 * v['D'] ** 2 / v['G'] - v['D'] * p[k] + 0.5 * v['G'] * p[k] ** 2, model_fix_t.nodes.items(), 0)
+    prt = reduce(lambda x, (k, v): x + (p[k[1]] - p[k[0]]) * q[k], model_fix_t.edges.items(), 0)
+    e = reduce(lambda x, (k, v): x + v['a'] * Q[k] ** 2 + v['b'] * Q[k] + v['c'], model_fix_t.edges.items(), 0)
+    tw = 24 * (pr + cs + prt) - e
+    return {
+        'gradient': {k: 24 * (p[k[1]] - p[k[0]]) - (2 * v['a'] * Q[k] + v['b']) for k, v in model.edges.items()},
+        't': t,
+        'tw': tw
+    }
 
 
 def start_external_procedure(model, eps, c, projector=lambda x: x):
@@ -19,10 +28,11 @@ def start_external_procedure(model, eps, c, projector=lambda x: x):
 
     while not check_eps(q, q_prev, eps):
         q_prev = q
-        delta_tw = calc_delta_tw_stochastic_gradient(model, q, eps)
+        delta_tw_dict = calc_delta_tw_stochastic_gradient(model, q, eps)
+        delta_tw = delta_tw_dict['gradient']
         omega = get_current_omega(counter, c)
         q = projector({k: v + omega * delta_tw[k] for k, v in q_prev.items()})
         counter += 1
-        print q
+        print q, ' :: ', delta_tw_dict['tw']
 
     return q
