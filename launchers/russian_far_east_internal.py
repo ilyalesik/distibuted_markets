6a1ccf8dbeeec49e2df23fffc0091d1995a4f895
Ilia Lesik <ilialesik@gmail.com>
2018-11-12 15:14:34 +0300

    Simplify russian_far_east_internal

--- launchers/russian_far_east_internal.py
+++ launchers/russian_far_east_internal.py
@@ -3,21 +3,20 @@ from components.planning_external_procedure import start_external_procedure, get
 from launchers.russian_far_east_model import far_east_model
 
 T = 1
-t = 0
 eps = 0.001
-dQ = {
-    (2, 1): [1251.1],
-    (3, 1): [10606.6],
-    (4, 5): [6933.6],
-    (5, 3): [2205.2],
-    (7, 9): [704.2],
-    (1, 9): [1667.1],
-    (11, 10): [14010.6],
-    (1, 11): [1828.0],
-    (11, 12): [490.6]
+Q = {
+    (2, 1): 1251.1,
+    (3, 1): 10606.6,
+    (4, 5): 6933.6,
+    (5, 3): 2205.2,
+    (7, 9): 704.2,
+    (1, 9): 1667.1,
+    (11, 10): 14010.6,
+    (1, 11): 1828.0,
+    (11, 12): 490.6
 }
-model_fix_t = far_east_model.get_input_model_with_fix_t(t)
-Q_slice_t = get_q_slice(far_east_model, t, dQ)
-q = start_internal_procedure(model_fix_t, Q_slice_t, eps)
+model_fix_t = far_east_model.get_input_model_with_fix_t(0)
+q = start_internal_procedure(model_fix_t, Q, eps)
+print "q: ", q
 p = calc_p(model_fix_t, q)
-print p
+print "p: ", p
