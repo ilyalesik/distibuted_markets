660b5452428d6d46336060db97bb2c3b10732a14
Ilia Lesik <ilialesik@gmail.com>
2018-11-30 18:57:17 +0300

    Add constraints to inside procedure (G) for zero demand, zero supply cases

--- components/inside_procedure.py
+++ components/inside_procedure.py
@@ -10,6 +10,27 @@ def find_c(Q):
 def projector(q, Q_max):
     return {k: (-1 if v < 0 else 1) * min(abs(v), abs(Q_max[k])) if k in Q_max else v for k, v in q.items()}
 
+def find_min_g(model, q_model):
+    min_G = None
+    subgradient_G = None
+    for node_key, node in model.nodes.items():
+        g1 = None
+        g2 = None
+        if (node.D != 0.0 and node.G != 0.0 and node.A != 0.0 and node.B != 0.0):
+            g1 = q_model.get_sum(node_key) - node.G * node.B + node.D
+            g2 = 0.5 * (node.D / node.G - node.B) / node.A - q_model.get_sum(node_key)
+        elif (node.A == 0.0 and node.B == 0.0):
+            g1 = q_model.get_sum(node_key)
+        elif (node.D == 0.0 and node.G == 0.0):
+            g2 = -q_model.get_sum(node_key)
+        if (g1 < g2 or (g2 is None)) and ((min_G is None) or g1 < min_G):
+            min_G = g1
+            subgradient_G = q_model.get_subgradient(node_key)
+        elif (g2 < g1 or (g1 is None)) and ((min_G is None) or g2 < min_G):
+            min_G = g2
+            subgradient_G = q_model.get_subgradient(node_key)
+    return subgradient_G, min_G
+
 
 def get_p(i, q_model, node):
     p = 0.0
@@ -20,13 +41,16 @@ def get_p(i, q_model, node):
         p = (q + node.D) / node.G
     else:
         p = (q + 0.5 * node.B / node.A + node.D) / (0.5 / node.A + node.G)
-    return 0.0 if p < 0 else p
+    return p
 
 def get_gradient_w(model, q_model):
     return {k: get_p(k[1], q_model, model.nodes[k[1]]) - get_p(k[0], q_model, model.nodes[k[0]]) - v.e_tr for k, v in model.edges.items()}
 
 def get_S(model, q_model):
-    return get_gradient_w(model, q_model)
+    min_G = find_min_g(model, q_model)
+    if min_G[1] >= 0:
+        return get_gradient_w(model, q_model)
+    return min_G[0]
 
 
 def start_internal_procedure(model, Q_max, eps):
