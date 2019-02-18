a832955cef8d04ea2fa7177a5de5b376334583d6
Ilia Lesik <ilialesik@gmail.com>
2019-02-18 18:05:23 +0300

    Fix russian_far_east launcher

--- launchers/russian_far_east.py
+++ launchers/russian_far_east.py
@@ -4,6 +4,6 @@ from launchers.russian_far_east_model_reduced import far_east_model
 q_initial={(1, 8): 7000, (3, 1): 3300, (6, 1): 1500, (8, 9): 1400, (11, 10): 1400, (8, 11): 1900}
 
 T = 10
-q = start_external_procedure(far_east_model, T, 0.001, 0.1, 0.1, q_initial)
+q = start_external_procedure(far_east_model, T, 0.001, 0.1, 0.1, 0.01, q_initial)
 print '----------------------'
 print q
