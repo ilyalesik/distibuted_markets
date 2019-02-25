49fcf4847c791f3b821989ee5fb54d751bbde1e5
Ilia Lesik <ilialesik@gmail.com>
2019-02-25 21:25:03 +0300

    Remove unused launchers

--- launchers/russian_far_east_1_9.py
+++
@@ -1,7 +0,0 @@
-from components.planning_external_procedure import start_external_procedure
-from launchers.russian_far_east_model_1_9 import far_east_model
-
-T = 1
-q = start_external_procedure(far_east_model, T, 0.0001, 0.1)
-print '----------------------'
-print q
